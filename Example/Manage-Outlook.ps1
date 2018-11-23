c510ebe23fc594bca38c54598c3f6fd3d1b48d83
PrzemyslawKlys <pklys@evotec.pl>
2018-11-23 22:35:55 +0100

    Update

--- Example/Manage-Outlook.ps1
+++ Example/Manage-Outlook.ps1
@@ -2,6 +2,6 @@ Clear-Host
 
 Import-Module PSOutlookProfile -Force
 
-Start-OutlookProfile -WhatIf -RemoveAccount 'przemyslaw.klys@euvic.pl' -PrimaryAccount 'przemyslaw.klys@evotec.pl'
+Start-OutlookProfile -WhatIf -RemoveAccount 'przemyslaw.klys@domain.pl' -PrimaryAccount 'przemyslaw.klys@evotec.pl'
 
-#Start-OutlookProfile -NoBackup -WhatIf -RemoveAccount 'przemyslaw.klys@euvic.pl' -PrimaryAccount 'przemyslaw.klys@evotec.pl'
+Start-OutlookProfile -NoBackup -WhatIf -RemoveAccount 'przemyslaw.klys@domain.pl' -PrimaryAccount 'przemyslaw.klys@evotec.pl'
